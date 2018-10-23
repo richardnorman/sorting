500e5214ba9a63f68675cd7298356436639cf636
Richard Norman <40871128+richardnorman@users.noreply.github.com>
2018-10-22 23:17:05 -0600

    Insertion sort complete

---
+++ Insertion_Sort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Insertion_Sort
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            //create an array with a random size fror 5-15
+            var randomArray = new int[random.Next(5,15)];
+
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                //fill array with random integers ranging from 1 to 99
+                randomArray[i] = random.Next(1, 100);
+            }
+
+            //print unsorted array for reference
+            Console.WriteLine("The unsorted array is: " + string.Join(" ", randomArray));
+
+            //now that we have a full array, begin insertion sort
+            for (var i = 1; i < randomArray.Length; i++)
+            {
+                int j = i;
+                while (randomArray[j] < randomArray[j-1])
+                {
+                    //store temporary value and swap if element on left is larger than selected element
+                    int temp = randomArray[j-1];
+                    randomArray[j - 1] = randomArray[j];
+                    randomArray[j] = temp;
+                    j -= 1;
+                    //prevents out of bounds exception, occurs when smallest number in array is sorted
+                    if (j == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            //print sorted array
+            Console.WriteLine("The sorted array is: " + string.Join(" ", randomArray));
+        }
+    }
+}
